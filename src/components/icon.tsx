19075ca360fdf941a9c36c6415c95bafaaf482e7
Vitorcd20 <vitorvieeira12008@gmail.com>
2025-10-27 14:14:25 -0300

    feat: add icon component

---
+++ src/components/icon.tsx
@@ -0,0 +1,32 @@
+import { cva, type VariantProps } from "class-variance-authority";
+import { twMerge } from "tailwind-merge";
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const iconVariants = cva("", {
+  variants: {
+    animate: {
+      true: "animate-spin",
+      false: "",
+    },
+  },
+  defaultVariants: {
+    animate: false,
+  },
+});
+
+interface IconProps
+  extends React.ComponentProps<"svg">,
+    VariantProps<typeof iconVariants> {
+  svg: React.FC<React.ComponentProps<"svg">>;
+}
+
+export default function Icon({
+  svg: SvgComponent,
+  animate,
+  className,
+  ...props
+}: IconProps) {
+  return (
+    <SvgComponent className={twMerge(iconVariants({ animate, className }))} {...props} />
+  );
+}
