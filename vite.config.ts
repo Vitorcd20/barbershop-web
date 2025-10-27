57502b4439c4ed3dc322638096b121a924acbe59
Vitorcd20 <vitorvieeira12008@gmail.com>
2025-10-27 13:50:21 -0300

    installing few dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), svgr()],
   server: {
-    host: true, 
+    host: true,
     port: 5173,
   },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  }
+  },
 });
