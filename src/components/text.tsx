78ab2fbe8d20dd4329288dbf741b046c9e937ae1
Vitorcd20 <vitorvieeira12008@gmail.com>
2025-10-27 14:14:14 -0300

    feat: add text component

---
+++ src/components/text.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+import { twMerge } from "tailwind-merge";
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const textVariants = cva("font-sans text-gray-200", {
+  variants: {
+    variant: {
+      "title-lg": "font-sans text-2xl/8 font-bold",
+      "title-md": "font-sans text-base/6 font-bold",
+      "title-sm": "font-sans text-sm/5 font-bold",
+      "text-md": "font-sans text-base/6 font-normal",
+      "text-sm": "font-sans text-sm/5 font-normal",
+    },
+  },
+  defaultVariants: {
+    variant: "text-md",
+  },
+});
+
+interface TextProps extends VariantProps<typeof textVariants> {
+  as?: keyof React.JSX.IntrinsicElements;
+  className?: string;
+  children?: React.ReactNode;
+}
+
+export default function Text({
+  as = "span",
+  variant,
+  className,
+  children,
+  ...props
+}: TextProps) {
+  return React.createElement(
+    as,
+    {
+      className: twMerge(textVariants({ variant, className })),
+      ...props,
+    },
+    children
+  );
+}
