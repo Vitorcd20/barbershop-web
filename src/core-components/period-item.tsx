c73181be75256a8f18b4c683519144bd986d83bd
Vitorcd20 <vitorvieeira12008@gmail.com>
2025-10-27 19:40:19 -0300

    feat: add period-item

---
+++ src/core-components/period-item.tsx
@@ -0,0 +1,32 @@
+import ButtonIcon from "../components/button-icon";
+import Trash from "../assets/icons/Trash.svg?react";
+import Text from "../components/text";
+import { useAppointment } from "../hooks/use-appointment";
+
+type Props = {
+  id: string;
+  time: string;
+  client: string;
+};
+
+export default function PeriodList({ client, id, time }: Props) {
+  const { deleteAppointment } = useAppointment();
+
+  function handleDelete(id: string) {
+    deleteAppointment(id);
+  }
+
+  return (
+    <li>
+      <Text variant="title-md">{time}</Text>
+      <Text variant="text-md" className="w-full">
+        {client}
+      </Text>
+      <ButtonIcon
+        className="shrink-0 fill-yellow"
+        icon={Trash}
+        onClick={() => handleDelete(id)}
+      />
+    </li>
+  );
+}
