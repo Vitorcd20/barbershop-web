703f86463e16c9431e80aab6367acb8a8ea51f1b
Vitorcd20 <vitorvieeira12008@gmail.com>
2025-10-27 14:14:47 -0300

    feat: add text-input component

---
+++ src/components/text-input.tsx
@@ -0,0 +1,43 @@
+import { cva, type VariantProps } from "class-variance-authority";
+import { textVariants } from "./text";
+import Icon from "./icon";
+import { twMerge } from "tailwind-merge";
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const textInputVariantsContainer = cva(
+  "has-[:focus]:border-yellow-dark transition-colors p-3 rounded-xl border border-gray-500 flex items-center justify-center gap-2"
+);
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const textInputVariantsIcon = cva("size-5 fill-yellow");
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const textInputVariants = cva(
+  "w-full outline-none text-gray-200 placeholder:text-gray-400"
+);
+
+interface TextInputProps
+  extends React.ComponentProps<"input">,
+    VariantProps<typeof textInputVariants> {
+  icon: React.ComponentProps<typeof Icon>["svg"];
+}
+
+export default function TextInput({
+  className,
+  icon,
+  ...props
+}: TextInputProps) {
+  return (
+    <label className={twMerge(textInputVariantsContainer({ className }))}>
+      <Icon svg={icon} className={textInputVariantsIcon()} />
+      <input
+        type="text"
+        className={twMerge(textVariants({
+          variant: "text-md",
+          className: textInputVariants(),
+        }))}
+        {...props}
+      />
+    </label>
+  );
+}
